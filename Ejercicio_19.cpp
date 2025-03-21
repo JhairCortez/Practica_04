4a91aa53c8775dec77156888df1eef7f0e6c0814
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-21 00:03:25 -0400

    Update Ejercicio_19.cpp

--- Ejercicio_19.cpp
+++ Ejercicio_19.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+void serie_mercene(int n)
+{
+    int a = 1, b = 1, suma = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a << endl;
+        suma += a;
+        int temp = a;
+        a = a + b;
+        b = temp;
+    }
+    cout << "Sumatoria de los mercene: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_mercene(n);
+    return 0;
+}
