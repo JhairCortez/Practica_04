e1383c73f59e9f579b7143a61c49395ecc6c2aa5
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:56:04 -0400

    Update Ejercicio_08.cpp

--- Ejercicio_08.cpp
+++ Ejercicio_08.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+int potencia(int b, int e)
+{
+    int p = 1;
+    for(int i = 1; i <= e; i++)
+    {
+        p *= b;
+    }
+    return p;
+}
+int main()
+{
+    int b, e;
+    cout << "Ingrese la base: ";
+    cin >> b;
+    cout << "Ingrese el exponente: ";
+    cin >> e;
+    cout << "Resultado: " << potencia(b, e) << endl;
+    return 0;
+}
