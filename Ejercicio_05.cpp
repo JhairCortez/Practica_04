b60841538be26072268c6c54d6a9b5ba5b700720
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:53:47 -0400

    Update Ejercicio_05.cpp

--- Ejercicio_05.cpp
+++ Ejercicio_05.cpp
@@ -1,1 +1,20 @@
-
+#include <iostream>
+using namespace std;
+void serie(int n)
+{
+    int signo = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i * signo << endl;
+        if(i % 2 == 0)
+            signo = -signo;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Ingrese el valor de N: ";
+    cin >> n;
+    serie(n);
+    return 0;
+}
