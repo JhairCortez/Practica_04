0343dd9638d671e53f138083194cbbe95ce86f1a
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-21 00:02:02 -0400

    Update Ejercicio_17.cpp

--- Ejercicio_17.cpp
+++ Ejercicio_17.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+void serie_pell(int n)
+{
+    int a = 0, b = 1, suma = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a << endl;
+        suma += a;
+        int temp = a;
+        a = 2 * b + a;
+        b = temp;
+    }
+    cout << "Sumatoria de los pell: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_pell(n);
+    return 0;
+}
