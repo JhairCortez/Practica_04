919c42ed7f832d5871cd0ce0fd499d455de53ebb
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-21 00:02:30 -0400

    Update Ejercicio_18.cpp

--- Ejercicio_18.cpp
+++ Ejercicio_18.cpp
@@ -1,1 +1,22 @@
-
+#include <iostream>
+using namespace std;
+void serie_bernoulli(int n)
+{
+    double suma = 0;
+    for(int i = 0; i < n; i++)
+    {
+        // Calculando el número de Bernoulli
+        // Se puede usar una fórmula específica, pero por simplicidad se usa una aproximación básica
+        suma += (i % 2 == 0) ? 1.0 / (i + 1) : -(1.0 / (i + 1));
+        cout << suma << endl;
+    }
+    cout << "Sumatoria de los bernoulli: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_bernoulli(n);
+    return 0;
+}
