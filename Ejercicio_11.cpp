fdd58992b6c48849369614f3c15e7e2885213595
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:59:13 -0400

    Update Ejercicio_11.cpp

--- Ejercicio_11.cpp
+++ Ejercicio_11.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+void serie_cubos(int n)
+{
+    int suma = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        int cubo = i * i * i;
+        cout << cubo << endl;
+        suma += cubo;
+    }
+    cout << "Sumatoria de los cubos: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_cubos(n);
+    return 0;
+}
