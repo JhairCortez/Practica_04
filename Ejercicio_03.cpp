33633774ffd0c91a6b988ba1a78e7373d687b10b
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:52:42 -0400

    Update Ejercicio_03.cpp

--- Ejercicio_03.cpp
+++ Ejercicio_03.cpp
@@ -1,1 +1,13 @@
-
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a, b, r;
+    cout << "Ingrese dividendo: ";
+    cin >> a;
+    cout << "Ingrese divisor: ";
+    cin >> b;
+    r = a - (a / b) * b;
+    cout << "El residuo es: " << r << endl;
+    return 0;
+}
