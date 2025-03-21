1157a2eef032987bc66ef577b20e60cca531e3df
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-21 00:01:15 -0400

    Update Ejercicio_15.cpp

--- Ejercicio_15.cpp
+++ Ejercicio_15.cpp
@@ -1,1 +1,23 @@
-
+#include <iostream>
+using namespace std;
+void serie_lucas(int n)
+{
+    int a = 2, b = 1, suma = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << a << endl;
+        suma += a;
+        int temp = a;
+        a = a + b;
+        b = temp;
+    }
+    cout << "Sumatoria de los lucas: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_lucas(n);
+    return 0;
+}
