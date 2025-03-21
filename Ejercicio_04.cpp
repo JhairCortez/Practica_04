08bcba73f1da0b23bd943bb866fca6719b98bedb
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:53:16 -0400

    Update Ejercicio_04.cpp

--- Ejercicio_04.cpp
+++ Ejercicio_04.cpp
@@ -1,1 +1,17 @@
-
+#include <iostream>
+using namespace std;
+void multiples_de_tres(int n)
+{
+    for(int i = 1; i <= n; i++)
+    {
+        cout << i * 3 << endl;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Ingrese el valor de N: ";
+    cin >> n;
+    multiples_de_tres(n);
+    return 0;
+}
