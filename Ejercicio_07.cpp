877dfd87b5f4775a19736d6bb15d865f790c3848
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-20 23:54:54 -0400

    Update Ejercicio_07.cpp

--- Ejercicio_07.cpp
+++ Ejercicio_07.cpp
@@ -1,1 +1,19 @@
-
+#include <iostream>
+using namespace std;
+void serie(int n)
+{
+    int t = 1;
+    for(int i = 1; i <= n; i++)
+    {
+        cout << t << endl;
+        t += (i % 2 == 0) ? 4 : 5;
+    }
+}
+int main()
+{
+    int n;
+    cout << "Ingrese el valor de N: ";
+    cin >> n;
+    serie(n);
+    return 0;
+}
