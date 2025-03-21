25afd66ad538a1061928366aa497c70a456c65be
JhairCortezUcb <jhair.cortez@ucb.edu.bo>
2025-03-21 00:00:45 -0400

    Update Ejercicio_14.cpp

--- Ejercicio_14.cpp
+++ Ejercicio_14.cpp
@@ -1,1 +1,21 @@
-
+#include <iostream>
+using namespace std;
+void serie_triangular(int n)
+{
+    int suma = 0;
+    for(int i = 1; i <= n; i++)
+    {
+        int triangular = i * (i + 1) / 2;
+        cout << triangular << endl;
+        suma += triangular;
+    }
+    cout << "Sumatoria de los triangulares: " << suma << endl;
+}
+int main()
+{
+    int n;
+    cout << "Ingrese N: ";
+    cin >> n;
+    serie_triangular(n);
+    return 0;
+}
